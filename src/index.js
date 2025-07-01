60017fef562e6a4094b0a3cbc1e30cc7c5f82229
toan0456 <toanpdph42407@fpt.edu.vn>
2025-07-01 08:54:16 +0700

    Update tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // Đảm bảo file này sẽ chứa Tailwind directives
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
